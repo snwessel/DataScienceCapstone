ecffafa1c50e94f731ad1483676da0b1b489be65
snwessel <wessel.s@husky.neu.edu>
2021-03-01 16:57:12 -0500

    Simplify so much by using plotly instead of d3

--- app/models.py
+++ app/models.py
@@ -22,11 +22,13 @@ class DataLoader:
     # Query the CDC API
     client = Socrata("data.cdc.gov", None)
     results = client.get("9mfq-cb36", state=state_abbrev)
-    results_df = pd.DataFrame.from_records(results)
+    results_df = pd.DataFrame.from_records(results).sort_values(by=['created_at'])
     # return the new cases by date in a javascript-friendly format
-    cases_by_date_df = results_df[["new_case", "created_at"]]
-    cases_by_date_df = cases_by_date_df.sort_values(by=['created_at'])
-    return list(cases_by_date_df.T.to_dict().values())
+    cases_by_date_dict = {
+      "date": results_df["created_at"].tolist(),
+      "cases": results_df["new_case"].tolist()
+    }
+    return cases_by_date_dict
 
   def get_daily_vaccinations(state_abbrev):
     """Load daily vaccination counts from CSV"""
