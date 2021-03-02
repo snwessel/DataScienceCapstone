6a5759c54bcb5d42812c3d9385f237d4c7bb49ce
snwessel <wessel.s@husky.neu.edu>
2021-03-02 13:44:08 -0500

    Refactor and add MLModels interface

--- app/models.py
+++ app/models.py
@@ -1,36 +1,12 @@
-import csv
-import pandas as pd
-from sodapy import Socrata
-
-# Class definitions for any python utils
-
-class DataLoader:
-
-  def get_states():
-    """Load state abbreviations and names from file"""
-    # states will be stored in a dict 
-    # with the name as the key and the abbreviation as the value
-    states_dict = {}
-    with open('data/states.tsv', newline='') as states_file:
-      tsv_reader = csv.reader(states_file, delimiter='\t')
-      for row in tsv_reader:
-        states_dict[row[0]] = row[1]
-    return states_dict
-
-  def get_daily_cases(state_abbrev):
-    """Load daily case counts from the CDC API"""
-    # Query the CDC API
-    client = Socrata("data.cdc.gov", None)
-    results = client.get("9mfq-cb36", state=state_abbrev)
-    results_df = pd.DataFrame.from_records(results).sort_values(by=['created_at'])
-    # return the new cases by date in a javascript-friendly format
-    cases_by_date_dict = {
-      "date": results_df["created_at"].tolist(),
-      "cases": results_df["new_case"].tolist()
-    }
-    return cases_by_date_dict
-
-  def get_daily_vaccinations(state_abbrev):
-    """Load daily vaccination counts from CSV"""
-    # TODO: implement
+# An interface for interacting with machine learning models
+class MLModel:
+  def fit(daily_cases, daily_vaccinations):
+    """Train the model to fit the data"""
     pass
+
+  def predict(days):
+    """Predict the given number of days"""
+    # We may need to pass in the state data too
+    pass 
+
+# TODO: make classes that implement this interface
--- app/views.py
+++ app/views.py
@@ -1,14 +1,14 @@
-from app import app, models
+from app import app, models, data_loader
 from flask import render_template
 
 @app.route("/")
 def index():
-  states = models.DataLoader.get_states()
-  daily_cases = models.DataLoader.get_daily_cases("MA") # TODO: update this to get nationwide results
+  states = data_loader.DataLoader.get_states()
+  daily_cases = data_loader.DataLoader.get_daily_cases("MA") # TODO: update this to get nationwide results
   return render_template('index.html', state=None, states=states, daily_cases=daily_cases)
 
 @app.route("/state/<state>")
 def index_state(state):
-  states = models.DataLoader.get_states()
-  daily_cases = models.DataLoader.get_daily_cases(state)
+  states = data_loader.DataLoader.get_states()
+  daily_cases = data_loader.DataLoader.get_daily_cases(state)
   return render_template('index.html', state=state, states=states, daily_cases=daily_cases)
