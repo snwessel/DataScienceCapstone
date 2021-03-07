7443aca6a814f0a9ba3f448318bc8a1a66905a9c
oliviapetrillo <petrillo.o@husky.neu.edu>
2021-03-07 18:50:38 -0500

    Added support for vaccination data in visualizations

--- app/static/js/prediction_visualization.js
+++ app/static/js/prediction_visualization.js
@@ -1,15 +1,45 @@
 
-function render_prediction_visualization(dailyCases) {
-  var data = [
-    {
-      x: dailyCases['date'],
-      y: dailyCases['cases'],
-      type: 'scatter'
-    }
-  ];
+function render_prediction_visualization(dailyCases, dailyVaccinations) {
+  var caseData ={
+    x: dailyCases['date'],
+    y: dailyCases['cases'],
+    type: 'scatter',
+    mode: 'lines',
+    line: {
+      dash: 'solid'
+    },
+    name: 'daily cases'
+  };
+
+  var vaxData = {
+    x: dailyVaccinations['date'], 
+    y: dailyVaccinations['vaccinations'], 
+    type: 'scatter', 
+    mode: 'lines',
+    line: {
+      dash: 'solid'
+    },
+    name: 'daily vaccinations'
+  };
+
+  // fake temporary data to play around with
+  var futureData = {
+    x: ['2021-03-07', '2021-03-08', '2021-03-09', '2021-03-10', '2021-03-11', '2021-03-12', '2021-03-13', '2021-03-14', 
+    '2021-03-15', '2021-03-16', '2021-03-17', '2021-03-18', '2021-03-19', '2021-03-20', '2021-03-21', '2021-03-22', '2021-03-23'],
+    y: ['0', '1000', '2000', '3000', '4000', '5000', '6000', '7000', '8000', '9000', '10000', '11000', '12000', '13000', '14000', '15000', '16000'],
+    type: 'scatter',
+    mode: 'lines',
+    line: {
+      dash: 'dot'
+    },
+    name: 'predicted ???'
+  };
+
+  // list of all data series/"traces" (according to plotly)
+  var data = [caseData, vaxData, futureData];
 
   var layout = { 
-    title: 'Daily Covid Cases'
+    title: 'Daily Covid Cases vs Daily Vaccinations + Predictions'
   };
   
   var config = {responsive: true};
--- app/views.py
+++ app/views.py
@@ -4,11 +4,16 @@ from flask import render_template
 @app.route("/")
 def index():
   states = data_loader.DataLoader.get_states()
-  daily_cases = data_loader.DataLoader.get_daily_cases_dict("MA") # TODO: update this to get nationwide results
-  return render_template('index.html', state=None, states=states, daily_cases=daily_cases)
+  daily_cases = data_loader.DataLoader.get_national_cases_dict() # TODO: see if this is correct?
+  daily_vaccinations = data_loader.DataLoader.get_daily_vaccinations_dict("US")
+  return render_template('index.html', state=None, states=states, daily_cases=daily_cases, daily_vaccinations=daily_vaccinations)
 
 @app.route("/state/<state>")
 def index_state(state):
   states = data_loader.DataLoader.get_states()
-  daily_cases = data_loader.DataLoader.get_daily_cases_dict(state)
-  return render_template('index.html', state=state, states=states, daily_cases=daily_cases)
+  if state == "US":
+    daily_cases = data_loader.DataLoader.get_national_cases_dict()
+  else:
+    daily_cases = data_loader.DataLoader.get_daily_cases_dict(state)
+  daily_vaccinations = data_loader.DataLoader.get_daily_vaccinations_dict(state)
+  return render_template('index.html', state=state, states=states, daily_cases=daily_cases, daily_vaccinations=daily_vaccinations)
