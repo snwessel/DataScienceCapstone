5dbee147ea7f9545373052a5d3dccb22975b1345
oliviapetrillo <petrillo.o@husky.neu.edu>
2021-03-17 15:28:29 -0400

    Added support for basic flu/population visualizations

---
+++ app/static/js/influenza_visualization.js
@@ -0,0 +1,38 @@
+
+function render_influenza_visualization(influenzaData) {
+    var fluData ={
+      x: influenzaData['epiweek'],
+      y: influenzaData['wili'],
+      type: 'scatter',
+      mode: 'lines',
+      line: {
+        dash: 'solid'
+      },
+      name: 'actual'
+    };
+  
+    // fake temporary data to play around with
+    // var futureData = {
+    //   x: ['2021-03-07', '2021-03-08', '2021-03-09', '2021-03-10', '2021-03-11', '2021-03-12', '2021-03-13', '2021-03-14', 
+    //   '2021-03-15', '2021-03-16', '2021-03-17', '2021-03-18', '2021-03-19', '2021-03-20', '2021-03-21', '2021-03-22', '2021-03-23'],
+    //   y: ['0', '1000', '2000', '3000', '4000', '5000', '6000', '7000', '8000', '9000', '10000', '11000', '12000', '13000', '14000', '15000', '16000'],
+    //   type: 'scatter',
+    //   mode: 'lines',
+    //   line: {
+    //     dash: 'dot'
+    //   },
+    //   name: 'predicted'
+    // };
+  
+    // list of all data series/"traces" 
+    var data = [fluData];
+  
+    var layout = { 
+      title: '2020 FluView Influenza Data',
+      paper_bgcolor: '#fafaee'
+    };
+    
+    var config = {responsive: true};
+  
+    Plotly.newPlot('influenza_dataviz', data, layout, config);
+  }
--- app/views.py
+++ app/views.py
@@ -16,11 +16,15 @@ def index():
   daily_vaccinations_df = data_loader.DataLoader.get_daily_vaccinations_df(state)
   daily_vaccinations_dict = data_loader.DataLoader.get_daily_vaccinations_dict(daily_vaccinations_df, state)
   future_vaccinations = data_loader.DataLoader.get_assumed_vaccinations_dict(daily_vaccinations_df, multiplier=multiplier)
+  state_population_dict = data_loader.DataLoader.get_state_population_counts_dict()
+  influenza_dict = data_loader.DataLoader.get_infuenza_counts_dict()
   return render_template('index.html', 
                           state=state, 
                           multiplier=multiplier,
                           all_states=all_states, 
                           daily_cases=daily_cases_dict, 
                           daily_vaccinations=daily_vaccinations_dict,
-                          future_vaccinations=future_vaccinations)
+                          future_vaccinations=future_vaccinations,
+                          state_pop=state_population_dict,
+                          influenza_counts=influenza_dict)
 
