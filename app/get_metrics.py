f78e6ac4de0884bd7d38dd741011317d514e9e2a
snwessel <wessel.s@husky.neu.edu>
2021-04-01 23:12:04 -0400

    Give up on fixing the deployed config file

---
+++ app/get_metrics.py
@@ -0,0 +1,10 @@
+
+# get metrics for the multi-day predictions
+
+# Load the training data with n days held out
+
+# train the model 
+
+# get the testing data using the new function I made
+
+# get predictions 
